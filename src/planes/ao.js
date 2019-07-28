3d7e0c4e37af17c5c48df75d83a3e2330b9cb2a8
onecastell <bloofrank55@gmail.com>
2019-07-28 09:27:22 -0400

    Added new base plane module

---
+++ src/planes/ao.js
@@ -0,0 +1,53 @@
+import * as THREE from 'three'
+import OrbitControls from 'three-orbitcontrols'
+
+const scene = new THREE.Scene()
+const [width, height] = [window.innerWidth, window.innerHeight]
+
+// PLANE SURFACE
+
+// Individual Line Class
+class line {
+    constructor(p1, p2) {
+        const material = new THREE.LineBasicMaterial({ color: 0x0000ff, linewidth: 1 })
+        const geometry = new THREE.BufferGeometry()
+        const vertices = new Float32Array(p1.concat(p2))
+        geometry.addAttribute('position', new THREE.BufferAttribute(vertices, 3))
+
+        return new THREE.Line(geometry, material)
+    }
+}
+
+const bounds = 5 // Specifies grid area where area = (-bounds <= 0 <= bounds) ^ 2
+let horizontals = []
+let verticals = []
+
+// Generate Grid
+for (let i = -bounds; i <= bounds; i++) {
+    horizontals.push(new line([-bounds, 0, i], [bounds, 0, i]))
+}
+for (let i = -bounds; i <= bounds; i++) {
+    verticals.push(new line([i, 0, -bounds], [i, 0, bounds]))
+}
+
+scene.add(...horizontals, ...verticals)
+
+// LIGHTS
+const lights = []
+
+// CAMERA
+const camera = new THREE.PerspectiveCamera(60, width / height, 1, 500)
+camera.lookAt(0, 0, 0)
+camera.position.set(6.2, Math.PI / 1.5, 7.5)
+
+// ACTION
+const canvas = document.querySelector('canvas')
+const renderer = new THREE.WebGLRenderer({ canvas: canvas, antialias: true })
+renderer.setSize(width, height)
+
+new OrbitControls(camera, canvas)
+
+    ; (function animate() {
+        renderer.render(scene, camera)
+        window.requestAnimationFrame(animate)
+    })()
--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ var HtmlWebpackInlineSourcePlugin = require('html-webpack-inline-source-plugin')
 var path = require('path')
 
 module.exports = {
-    entry: './src/loader.js',
+    entry: './src/planes/ao.js',
     output: {
         path: path.resolve(__dirname, './dist'),
         filename: 'main.js'
