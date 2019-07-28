26d846448326db94719be3dff2aa7e83c346f280
onecastell <bloofrank55@gmail.com>
2019-07-27 23:33:14 -0400

    Enhanced loading animation and code cleanup

--- src/loader.js
+++ src/loader.js
@@ -4,77 +4,65 @@ import * as TWEEN from '@tweenjs/tween.js'
 
 const scene = new THREE.Scene()
 scene.background = new THREE.Color(0x00ccff)
+
 const dLight = (x, y, z) => {
     const l = new THREE.DirectionalLight(0xffffff, 0.5)
     l.position.set(x, y, z)
     scene.add(l)
 }
-
-
 dLight(0, 0, 5)
 dLight(0, 0, -5)
-const [width, height] = [window.innerWidth, window.innerHeight]
-const camera = new THREE.PerspectiveCamera(45, width / height, 1, 500)
-camera.lookAt(0, 0, 0);
-camera.position.z = 15;
 
+const [width, height] = [window.innerWidth, window.innerHeight]
+const camera = new THREE.PerspectiveCamera(60, width / height, 1, 500)
+camera.lookAt(0, 0, 0);
+
+camera.position.z = 25
 var positions = new Float32Array([
     -1.0, -1.0, 3.0,
     1.0, -1.0, 3.0,
     1.0, 1.0, 3.0,
     -1.0, 1.0, 3.0,
-
     1.0, -1.0, 1.0,
     1.0, 1.0, 1.0,
     -1.0, 1.0, 1.0,
     -1.0, -1.0, 1.0
 ])
 
-
 const sphereGeometry = new THREE.SphereGeometry(.5, 25, 25, 0, 2 * Math.PI)
 var geometry = new THREE.BufferGeometry();
 geometry.addAttribute('position', new THREE.BufferAttribute(positions, 3));
 // let material = new THREE.PointsMaterial({ color: 0xa5a5a5, size: .1 });
-const material = new THREE.MeshPhongMaterial({ color: 0xa5a5a5 })
+const material = new THREE.MeshPhongMaterial({ color: 0xffffff })
 let point = new THREE.Points(geometry, material);
 
-// Plot pos and add to scene
+// Plot points, set tweens and add to scene
 for (let i = 0; i < positions.length; i += 3) {
     const dot = new THREE.Mesh(sphereGeometry, material)
-    dot.position.set(-10, 0, 0)
-    // dot.position.set(
-    //     positions[i],
-    //     positions[i + 1],
-    //     positions[i + 2],
-    // )
+    dot.position.set(-10 + i, 0, 0)
+
     const separate = new TWEEN.Tween(dot.position)
-        // .to({ x: positions[i], y: positions[i + 1], z: positions[i + 2] }, 1000)
-        .to({ x: i * .5, y: Math.sin(i),z:Math.sin(i)}, 2000)
+        .to({ y: 1, }, 400)
+        .delay((i + 3) * 10)
         .yoyo(true)
-        .easing(TWEEN.Easing.Quadratic.In)
+        .easing(TWEEN.Easing.Sinusoidal.InOut)
         .repeat(Infinity)
         .start();
 
     scene.add(dot)
 }
 
-
 let moveCam = speed => { camera.position.x = -Math.tan(speed) * 5; }
-// Delay cam translation 
-// let moveCam = () => { };
-// setTimeout(() => {
-//     moveCam = speed => { camera.position.x = -Math.tan(speed) * 5; }
-// }, 2000);
+
 const canvas = document.querySelector('canvas')
 new OrbitControls(camera, canvas)
 const renderer = new THREE.WebGLRenderer({ canvas: canvas, antialias: true })
 renderer.setSize(width, height)
-camera.position.z = 25
 
     ; (function animate() {
         var speed = Date.now() * 0.001;
-        // moveCam(speed)
-        // camera.position.z = Math.sin(speed) * 5 + 20;
+        moveCam(speed)
+        // camera.position.z = -Math.sin(speed) * 5 + 20;
         TWEEN.update()
         window.requestAnimationFrame(animate)
         renderer.render(scene, camera)
