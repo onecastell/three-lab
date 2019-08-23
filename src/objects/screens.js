8a4a593f33a30f055fb5e42f0527706961280fdf
Franklin Castellanos <fcastellanos@mvixsusa.com>
2019-08-23 18:35:00 -0400

    Improve screen carousel animation

--- src/objects/screens.js
+++ src/objects/screens.js
@@ -4,18 +4,71 @@ const TWEEN = require('@tweenjs/tween.js')
 // Animated array of screens
 class screens {
     constructor() {
+
+        this.group = new THREE.Group()
+        this.screens = new THREE.Group()
+
         const width = 4
         const height = 3
         const duration = 1000
 
         const screenGeometry = new THREE.BoxBufferGeometry(width, .1, height)
-        const screenMaterial = new THREE.MeshLambertMaterial({ color: 0x353535, transparent: true })
-        screenMaterial.opacity = 0
 
-        // Top Screen
-        const screen = new THREE.Mesh(screenGeometry, screenMaterial)
-        screen.castShadow = true
-        screen.position.set(0, .6, -.5)
+        let screenYpos = .9
+        let screenIndex = 1
+        // Generate array of stacked screens
+        for (let i = 0; i < 4; i++) {
+            const screenMaterial = new THREE.MeshLambertMaterial({ color: 0x353535, transparent: true })
+            screenMaterial.opacity = 1
+            const screen = new THREE.Mesh(screenGeometry, screenMaterial)
+            screen.castShadow = false
+            // screen.position.set(0, .6, -.5)
+            screen.position.set(0, screenYpos -= .4, -.5)
+            this.screens.add(screen)
+        }
+
+        // Translate all screens up
+        this.next = () => {
+            const magnitude = this.screens.position.y + .4
+            console.log(magnitude, this.screens.children.length * .4)
+            if (this.screens.position.y + .4 < this.screens.children.length * .4) {
+                new TWEEN.Tween(this.screens.position)
+                    .to({ y: magnitude }, 1000)
+                    .delay(500)
+                    .easing(TWEEN.Easing.Back.Out)
+                    .start()
+                new TWEEN.Tween(this.screens.children[screenIndex].material)
+                    .to({ opacity: 1 }, 1000)
+                    .easing(TWEEN.Easing.Back.Out)
+                    .start()
+                new TWEEN.Tween(this.screens.children[screenIndex - 1].material)
+                    .to({ opacity: 0 }, 700)
+
+                    .delay(500)
+                    .start()
+                screenIndex += 1
+                console.log(screenIndex)
+            }
+        }
+
+        // Translate all screens down
+        this.prev = () => {
+            const magnitude = this.screens.position.y
+            console.log(magnitude)
+            if (this.screens.position.y >=.4) {
+                new TWEEN.Tween(this.screens.position)
+                    .to({ y: magnitude - .4 }, 1000)
+                    .delay(500)
+                    .easing(TWEEN.Easing.Back.Out)
+                    .start()
+                new TWEEN.Tween(this.screens.children[screenIndex-2].material)
+                    .to({ opacity: 1 }, 700)
+                    .easing(TWEEN.Easing.Back.Out)
+                    .delay(500)
+                    .start()
+                screenIndex -= 1
+            }
+        }
 
         // Bottom Screen
         const bottomScreenMaterial = new THREE.MeshLambertMaterial({ color: 0x353535, transparent: true })
@@ -45,23 +98,22 @@ class screens {
         this.video.autoplay = false
         this.video.loop = true
 
-        
-        const videoGeometry = new THREE.PlaneBufferGeometry(width-.05, height-1)
+        const videoGeometry = new THREE.PlaneBufferGeometry(width - .05, height - 1)
         const videoTexture = new THREE.VideoTexture(this.video);
         videoTexture.minFilter = THREE.LinearFilter;
         videoTexture.magFilter = THREE.LinearFilter;
         videoTexture.format = THREE.RGBFormat;
-        const videoMaterial = new THREE.MeshPhongMaterial({map:videoTexture})
-        const videoOverlay = new THREE.Mesh(videoGeometry,videoMaterial)
+        const videoMaterial = new THREE.MeshPhongMaterial({ map: videoTexture })
+        const videoOverlay = new THREE.Mesh(videoGeometry, videoMaterial)
         videoOverlay.position.set(0, .66, -.5)
-        videoOverlay.rotation.x = -Math.PI/2
-        
-        // Video focus animation 
+        videoOverlay.rotation.x = -Math.PI / 2
+
+        // Video focus animation
         new TWEEN.Tween(videoOverlay.scale)
-            .to({y:.85},2000)
+            .to({ y: .85 }, 2000)
             .delay(duration)
             .easing(TWEEN.Easing.Back.Out)
-            // .start()
+        // .start()
 
         // Start Screen Carousel Animation
         this.anim = () => {
@@ -102,8 +154,9 @@ class screens {
                 .start()
         }
 
-        this.group = new THREE.Group()
-        this.group.add(screen, bottomScreen,videoOverlay)
+        this.anim = () => { }
+
+        this.group.add(this.screens)
     }
 }
 
