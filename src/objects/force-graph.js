90a7f1ca614ea6ae86abb5b388315bf4508ff416
onecastell <bloofrank55@gmail.com>
2019-11-02 23:43:52 -0400

    Optimized node generation in force graph

--- src/objects/force-graph.js
+++ src/objects/force-graph.js
@@ -3,34 +3,39 @@ const TWEEN = require('@tweenjs/tween.js')
 
 class forceGraph {
     constructor(x, y, z) {
-        // Shpere
-        const sphereGeometry = new THREE.SphereBufferGeometry(0.15, 32, 32)
-        const sphereMaterial = new THREE.MeshPhongMaterial({ color: 0xffffff })
-        const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial)
-        sphere.position.set(x, y, z)
+        const points = [
+            x, y, z,
+            x + -0.3, y + 0.6, z + 0.3,
+            x + 0.15, y + 0.3, z + 0.6,
+            x + -0.75, y + 0, z + 0.9,
+            x + 0.3, y + 0, z + 1.5,
+            x + 1, y + 0.5, z + 0.5,
+        ]
 
-        // Line
+        // Lines
         const lineGeometry = new THREE.BufferGeometry()
         lineGeometry.addAttribute(
             'position',
             new THREE.BufferAttribute(
-                new Float32Array(
-                    [
-                        x , y, z,
-                        x - .35, y+ 1, z + .35
-                    ]
-                ), 3
+                new Float32Array([...points]), 3
             )
         )
-        const lines = []
-        const sphere2 = new THREE.Mesh(sphereGeometry,sphereMaterial)
-        sphere2.position.set(x - .35, y+ 1, z + .35)
+        const lineMaterial = new THREE.LineBasicMaterial({ color: 0xff0000, linewidth: 2 })
+        const lines = new THREE.Line(lineGeometry, lineMaterial)
 
-        const lineMaterial = new THREE.LineBasicMaterial({ color: 0xff0000,linewidth:2 })
-        const line = new THREE.Line(lineGeometry, lineMaterial)
+        // Shperes
+        const sphereGeometry = new THREE.SphereBufferGeometry(0.15, 32, 32)
+        const sphereMaterial = new THREE.MeshPhongMaterial({ color: 0xffffff })
+        const spheres = []
+        while (points.length > 0) {
+            const [x, y, z] = points.splice(0, 3)
+            const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial)
+            sphere.position.set(x, y, z)
+            spheres.push(sphere)
+        }
 
         this.group = new THREE.Group()
-        this.group.add(sphere,line,sphere2)
+        this.group.add(lines, ...spheres)
     }
 }
 
